ce88bde591fc76e90350e4221922e0d3c5436590
lb1213 <zhanggy@weeon.cn>
2018-06-12 17:09:22 +0800

    添加域名

--- app/src/api/api.js
+++ app/src/api/api.js
@@ -4,8 +4,8 @@ import Axios from 'axios';
 import router from '../router';
 
 
-let base ="http://127.0.0.1";
-//let base ="http://www.118-china.com";
+//let base ="http://127.0.0.1";
+let base ="http://www.118-china.com";
 //let base = 'http://'+ip+'/api/';
 //let base = 'http://192.168.100.105:9011/api/';
 export const getMeet = params => { return Axios.post(`${base}/weixin/getMeet.wei`,qs.stringify(params)).then(res => res.data); }; 
@@ -17,9 +17,6 @@ export const getTrainPlan = params => { return Axios.post(`${base}/weixin/getTra
 export const signTrainPlan = params => { return Axios.post(`${base}/weixin/signTrainPlan.wei`,qs.stringify(params)).then(res => res.data); }; 
 
  
-
-
-
 Axios.interceptors.request.use(
     config => {
         if (sessionStorage.getItem('accessToken')) {  // 判断是否存在token，如果存在的话，则每个http header都加上token
